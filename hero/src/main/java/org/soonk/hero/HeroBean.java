8997cbc4d7ebc616fe6f906e06c774d221f8af42
Scott Oonk <scott.oonk@gmail.com>
2016-01-09 12:55:31 -0500

    Define bean data

--- hero/src/main/java/org/soonk/hero/HeroBean.java
+++ hero/src/main/java/org/soonk/hero/HeroBean.java
@@ -2,4 +2,12 @@ package org.soonk.hero;
 
 public class HeroBean {
 
+	private String name;
+	private int cost;
+	private int will;
+	private int attack;
+	private int defense;
+	private int hitPoints;
+	private String sphere;
+	
 }
